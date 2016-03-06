88ca268710d6b9ff79f6499295d99b18537a7089
aheitzen <alexaheitzenrader@yahoo.com>
2016-03-05 16:58:52 -0800

    working on jquery plugins

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $('.slider').slick({ 
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  variableWidth: false,
+
+  });
+
+  $(".fancy_title").lettering();
+
+});
+
+
+
