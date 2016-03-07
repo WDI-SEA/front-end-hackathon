c45e872fb1602dac09b5c74d99e741b919cdbb60
aheitzen <alexaheitzenrader@yahoo.com>
2016-03-06 21:36:37 -0800

    fixed indentation

--- js/main.js
+++ js/main.js
@@ -1,16 +1,13 @@
 $(document).ready(function(){
   $('.slider').slick({ 
   	infinite: true,
-	  slidesToShow: 1,
-	  slidesToScroll: 1,
-	  variableWidth: false,
-	  dots: true,
-	  arrows: true
-
-  });
-
-
-  	$(".title").lettering();
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	variableWidth: false,
+	dots: true,
+	arrows: true
+});
+	$(".title").lettering();
  });
 
 
