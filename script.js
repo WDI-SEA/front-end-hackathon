b9cbb4f4c808f215dc82e8709c138ddf7c4d63fd
eun-n <eunicena.12@gmail.com>
2016-05-08 23:02:23 -0700

    styling

--- script.js
+++ script.js
@@ -5,18 +5,22 @@ var _lang = "likeLyrics";
 var title = {
   likeRom: "Like OOH-AHH", 
   likeLyrics: "OOH - AHH하게", 
-  ro: "Cupcakes"};
+  candyLyrics: "Candy Boy",
+  candyRom: "Candy Boy"
+};
  
 var lyrics = {
   likeRom: "modu nareul gajigo<br>maeil gaman an dujyo<br>naega neomu yeppeujyo<br>na ttaemune da himdeuljyo<br>eodil geotgo isseodo<br>ppalgan badagingeojyo<br>Red carpet gateun gibun<br>modu nal chyeoda bojyo oh<br>eotteon sarameun eomeonimi nugunyago<br>sinseonhage mal georeodo<br>amu neukkimi an deuljyo<br>hajiman nado nugunga hago sarange<br>ppajyeobogo sipeo baby<br>jal deureoyo nae boy<br>dan hanbeondo neukkyeobon jeok eopsneun geol<br>alge haejuneun<br>saram gidarigo issneun geol<br>eolmaga dwaedo gidarigo sipeo<br>I just wanna fall in love<br>eotteohge naega umjigil su eopsge<br>nal Ooh Ahh Ooh Ahh<br>hage mandeureojwo<br>gajja gajja jinsim eopsneun gajja<br>jal ga jal ga Huh<br>OOH AHHhage<br>eotteohge ije deo halmari eopsge<br>nal Ooh Ahh Ooh Ahh<br>hage mandeureojwo<br>Bla La La La malmanhaji malgo<br>neukkyeojige Huh<br>OOH AHHhage<br>nal bwa geobwa tto du beon bwa<br>hanbeon jinachigo<br>deungeul dollyeo chyeodabwa TWICE<br>eodil gadeorado hangsang min nacc<br>hajiman naega jeil biccna<br>najeun sinbal sineodo gachineun High<br>meorissogen neul yeonghwa sok gateun La La La<br>jangmyeondeuri jinagane<br>saenggakmanhaedo tteolline Yeah<br>ijeneun nado nugunga hago sarange<br>ppajyeo bogo sipeo baby<br>jal deureoyo nae boy<br>dan hanbeondo neukkyeobon jeok eopsneun geol<br>alge haejuneun<br>saram gidarigo issneun geol<br>eolmaga dwaedo gidarigo sipeo<br>I just wanna fall in love<br>eotteohge naega umjigil su eopsge<br>nal Ooh Ahh Ooh Ahh<br>hage mandeureojwo<br>gajja gajja jinsim eopsneun gajja<br>jal ga jal ga Huh<br>OOH AHHhage<br>eotteohge ije deo halmari eopsge<br>nal Ooh Ahh Ooh Ahh<br>hage mandeureojwo<br>Bla La La La malmanhaji malgo<br>neukkyeojige Huh<br>OOH AHHhage<br>amuhago manna sijakhagi silheo<br>swipji anheun yeoja geuge naingeol<br>Let me see<br>How you gon treat me<br>I ain’t no easy<br>Better think about it TWICE<br>Let me see<br>How you gon treat me<br>I ain’t no easy<br>Better think about it TWICE<br>eotteohge naega umjigil su eopsge<br>nal Ooh Ahh Ooh Ahh<br>hage mandeureojwo<br>gajja gajja jinsim eopsneun gajja<br>jal ga jal ga Huh<br>OOH AHHhage<br>eotteohge ije deo halmari eopsge<br>nal Ooh Ahh Ooh Ahh<br>hage mandeureojwo<br>Bla La La La malmanhaji malgo<br>neukkyeojige Huh<br>OOH AHHhage<br>OOH AHHhage ",
   likeLyrics: "모두 나를 가지고<br>매일 가만 안 두죠<br>내가 너무 예쁘죠<br>나 때문에 다 힘들죠<br>어딜 걷고 있어도<br>빨간 바닥인거죠<br>Red carpet 같은 기분<br>모두 날 쳐다 보죠 oh<br>어떤 사람은 어머님이 누구냐고<br>신선하게 말 걸어도<br>아무 느낌이 안 들죠<br>하지만 나도 누군가 하고 사랑에<br>빠져보고 싶어 Baby<br>잘 들어요 내 Boy<br>단 한번도 느껴본 적 없는 걸<br>알게 해주는<br>사람 기다리고 있는 걸<br>얼마가 돼도 기다리고 싶어<br>I just wanna fall in love<br>어떻게 내가 움직일 수 없게<br>날 Ooh Ahh Ooh Ahh<br>하게 만들어줘<br>가짜 가짜 진심 없는 가짜<br>잘 가 잘 가 Huh<br>OOH AHH하게<br>어떻게 이제 더 할말이 없게<br>날 Ooh Ahh Ooh Ahh<br>하게 만들어줘<br>Bla La La La 말만하지 말고<br>느껴지게 Huh<br>OOH AHH하게<br>날 봐 거봐 또 두 번 봐<br>한번 지나치고<br>등을 돌려 쳐다봐 TWICE<br>어딜 가더라도 항상 민 낯<br>하지만 내가 제일 빛나<br>낮은 신발 신어도 가치는 High<br>머릿속엔 늘 영화 속 같은 La La La<br>장면들이 지나가네<br>생각만해도 떨리네 Yeah<br>이제는 나도 누군가 하고 사랑에<br>빠져 보고 싶어 Baby<br>잘 들어요 내 Boy<br>단 한번도 느껴본 적 없는 걸<br>알게 해주는<br>사람 기다리고 있는 걸<br>얼마가 돼도 기다리고 싶어<br>I just wanna fall in love<br>어떻게 내가 움직일 수 없게<br>날 Ooh Ahh Ooh Ahh<br>하게 만들어줘<br>가짜 가짜 진심 없는 가짜<br>잘 가 잘 가 Huh<br>OOH AHH하게<br>어떻게 이제 더 할말이 없게<br>날 Ooh Ahh Ooh Ahh<br>하게 만들어줘<br>Bla La La La 말만하지 말고<br>느껴지게 Huh<br>OOH AHH하게<br>아무하고 만나 시작하기 싫어<br>쉽지 않은 여자 그게 나인걸<br>Let me see<br>How you gon treat me<br>I ain’t no easy<br>Better think about it TWICE<br>Let me see<br>How you gon treat me<br>I ain’t no easy<br>Better think about it TWICE<br>어떻게 내가 움직일 수 없게<br>날 Ooh Ahh Ooh Ahh<br>하게 만들어줘<br>가짜 가짜 진심 없는 가짜<br>잘 가 잘 가 Huh<br>OOH AHH하게<br>어떻게 이제 더 할말이 없게<br>날 Ooh Ahh Ooh Ahh<br>하게 만들어줘<br>Bla La La La 말만하지 말고<br>느껴지게 Huh<br>OOH AHH하게<br>OOH AHH하게",
-  ro: "Cupcake ipsum dolor sit amet. Toffee icing fruitcake dessert tart donut. Carrot cake tiramisu pudding. Lollipop cotton candy candy gummi bears cupcake pastry brownie. Powder candy canes cookie icing chocolate bar sesame snaps. Pudding sweet dessert cake. Cookie soufflé muffin. Cookie tiramisu gingerbread bear claw croissant gummies dessert sweet roll caramels. Soufflé jelly beans wafer macaroon. Cookie halvah sweet. Oat cake dessert sesame snaps. Liquorice cake wafer fruitcake gummies. Gummies jelly beans halvah candy canes bonbon."};
+  candyLyrics: "자다 깬 목소리<br>헝클어진듯한 Melody<br>캬라멜 Waterfall<br>몰래 둘이서 만났어<br>Ha 하늘엔 구름이<br>Everybody look at me 구름 위<br>뛰어 가다 넘어져도 좋아<br>너를 보러 가는 건 다 좋아<br>My Candy Boy My Baby<br>또 만났다고 말할까<br>My Candy Love My Baby<br>눈을 떠도 내 곁엔<br>너였으면 좋겠어<br>My Candy Boy My Baby<br>Gotta Have You Gotta Love You<br>Just be Mine Mine Mine<br>Baby Mine Mine Mine<br>Just be Mine Mine Mine<br>Baby Mine Mine Mine<br>무지개가 뜨는 밤 얘기<br>너랑 닿은 손이 뜨거웠지<br>입술 같은 건 너무 떨려<br>모르는 척 했어 바보같이<br>숨이 자꾸 멈추는 것 같아<br>어지러워 꿈꾸는 것 같아<br>My Candy Boy My Baby<br>또 만났다고 말할까<br>My Candy Love My Baby<br>꿈이 너무 달아서<br>눈을 떠도 내 곁엔<br>My Candy Boy My Baby<br>Gotta Have You Gotta Love You<br>Just be Mine Mine Mine<br>Baby Mine Mine Mine<br>Just be Mine Mine Mine<br>Baby Mine Mine Mine<br>Candy Boy<br>Gotta Have U Gotta Love U<br>Show Me Show Me Show Me<br>What U What U Got<br>Candy Boy<br>Gotta Have U Gotta Love U<br>Show Me Show Me Show Me<br>What U What U Got<br>My Candy Boy My Baby<br>또 만났다고 말할까<br>My Candy Love My Baby<br>꿈이 너무 달아서<br>눈을 떠도 내 곁엔<br>너였으면 좋겠어<br>My Candy Boy My Baby<br>Gotta Have You Gotta Love You<br>U My Candy Boy<br>U My Candy Boy",
+  candyRom: "jada kkaen moksori<br>heongkeureojindeushan Melody<br>kyaramel Waterfall<br>mollae duriseo mannasseo<br>Ha haneuren gureumi<br>Everybody look at me gureum wi<br>ttwieo gada neomeojyeodo joha<br>neoreul boreo ganeun geon da joha<br>My Candy Boy My Baby<br>tto mannassdago malhalkka<br>My Candy Love My Baby<br>kkumi neomu daraseo<br>nuneul tteodo nae gyeoten<br>neoyeosseumyeon johgesseo<br>My Candy Boy My Baby<br>Gotta Have You Gotta Love You<br>Just be Mine Mine Mine<br>Baby Mine Mine Mine<br>Just be Mine Mine Mine<br>Baby Mine Mine Mine<br>mujigaega tteuneun bam yaegi<br>neorang daheun soni tteugeowossji<br>ipsul gateun geon neomu tteollyeo<br>moreuneun cheok haesseo babogati<br>sumi jakku meomchuneun geot gata<br>eojireowo kkumkkuneun geot gata<br>My Candy Boy My Baby<br>tto mannassdago malhalkka<br>My Candy Love My baby<br>kkumi neomu daraseo<br>nuneul tteodo nae gyeoten<br>neoyeosseumyeon johgesseo<br>My Candy Boy My Baby<br>Gotta Have You Gotta Love You<br>Just be Mine Mine Mine<br>Baby Mine Mine Mine<br>Just be Mine Mine Mine<br>Baby Mine Mine Mine<br>Candy Boy<br>Gotta Have U Gotta Love U<br>Show Me Show Me Show Me<br>What U What U Got<br>Candy Boy<br>Gotta Have U Gotta Love U<br>Show Me Show Me Show Me<br>What U What U Got<br>My Candy Boy My Baby<br>tto mannassdago malhalkka<br>My Candy Love My Baby<br>kkumi neomu daraseo<br>nuneul tteodo nae gyeoten<br>neoyeosseumyeon johgesseo<br>My Candy Boy My Baby<br>Gotta Have You Gotta Love You<br>U My Candy Boy<br>U My Candy Boy"
+ }
 function start() {
  document.getElementById("Title").innerHTML = title[_lang];
  document.getElementById("Lyrics").innerHTML = lyrics[_lang];
 }
 
- function LangKo() {
+ function likeLyrics() {
 	_lang = "likeLyrics";
 	if(_lang == "likeLyrics") {
 	 document.getElementById("Title").innerHTML = title[_lang];
@@ -24,7 +28,7 @@ function start() {
 	}
 }
 
- function LangEn() {
+ function likeRom() {
 	_lang = "likeRom";
 	if(_lang == "likeRom") {
 	 document.getElementById("Title").innerHTML = title[_lang];
@@ -32,9 +36,17 @@ function start() {
 	}
 }
 
- function LangRo() {
-	_lang = "ro";
-	if(_lang == "ro") {
+function candyLyrics() {
+	_lang = "candyLyrics";
+	if(_lang == "candyLyrics") {
+	 document.getElementById("Title").innerHTML = title[_lang];
+ 	 document.getElementById("Lyrics").innerHTML = lyrics[_lang];
+	}
+}
+
+function candyRom() {
+	_lang = "candyRom";
+	if(_lang == "candyRom") {
 	 document.getElementById("Title").innerHTML = title[_lang];
  	 document.getElementById("Lyrics").innerHTML = lyrics[_lang];
 	}
