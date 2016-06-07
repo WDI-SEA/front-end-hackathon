5d9053620fd4834fd899fc03be2c7ae167fbd074
bengus101 <bgustafson29@yahoo.com>
2016-06-07 12:59:15 -0700

    Trying to get express working

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var session = require('express-session');
+var router = express.Router();
+
+var app = express();
+
+app.set('view engine', 'ejs');
 
 app.use(session({
-  secret: 'dsalkfjasdflkjgdfblknbadiadsnkl',
+  secret: 'hoo de lolly dont steal my stuff',
   resave: false,
   saveUninitialized: true
 }));
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.listen(3000);
