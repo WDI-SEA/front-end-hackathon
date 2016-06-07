516168cdfe42a6df0b13f07c053e139934eaa2f5
bengus101 <bgustafson29@yahoo.com>
2016-06-07 14:20:36 -0700

    Installed some external dependencies. Still working on getting the full page to render.

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 var express = require('express');
-// var ejsLayouts = require('express-ejs-layouts');
+var bodyParser = require('body-parser');
+var ejsLayouts = require('express-ejs-layouts');
 var session = require('express-session');
 var router = express.Router();
 
 var app = express();
 
 app.set('view engine', 'ejs');
-// app.use(ejsLayouts);
+app.use(ejsLayouts);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(__dirname + '/static'));
+
 
 app.use(session({
   secret: 'hoo de lolly dont steal my stuff',
