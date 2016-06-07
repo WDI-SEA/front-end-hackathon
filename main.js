c0b87dd2feade61c89337b81e107eac54f11106b
bengus101 <bgustafson29@yahoo.com>
2016-06-07 12:17:23 -0700

    Added a user into the db. Installed sessions

---
+++ main.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var session = require('express-session');
+
+app.use(session({
+  secret: 'dsalkfjasdflkjgdfblknbadiadsnkl',
+  resave: false,
+  saveUninitialized: true
+}));
