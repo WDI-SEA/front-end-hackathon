fc68d80873ae14f43c72d181c42dc1a0a9ca7edc
Jamie Karlovich <jamierkarlovich@gmail.com>
2016-07-10 22:30:04 -0700

    first 20 results showing, height in px of votes, alternating colors for candidates

--- js/primary.js
+++ js/primary.js
@@ -1,21 +1,22 @@
-// document.addEventListener("DOMContentLoaded", function(){
-//   function getData(){
-//   console.log(JSON.parse(this.responseText));
-//   };
-
-//   var oReq = new XMLHttpRequest();
-//   oReq.addEventListener("load", getData);
-//   oReq.open("GET", "js/data.json");
-//   oReq.send();
-// });
-
 $(document).ready (function () {
   console.log('ready');
 
-  $.getJSON('js/data.json', function(data) {
+  var data = [];
+
+  $.getJSON('js/data.json', function(primary) {
+
+    for (var i = 2; i < 100; i++) {
+        data.push([primary[i].Candidate, primary[i].PrecinctCode, primary[i].Votes]);
+    }
     console.log(data[0]);
 
-    // return data
+    d3.select('body').selectAll('div').data(data).enter().append('div').attr('class', 'bar').style('height', function(d){return d[2] +'px';}).style('background-color', function(d){
+        if(d[0] === 'Hillary Clinton') {
+          return 'red';
+        } else {
+          return 'blue';}
+      });
+
   });
-// console.log(data[0]);
+
 });
