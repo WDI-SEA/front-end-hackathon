90c3a9f83048770123c8a0c5413d5e16f31fca2e
Daniel Rasmussen <dan.r.rasmussen@gmail.com>
2016-07-10 20:28:24 -0700

    trying to combine vue.js with lettering.js

--- js/app.js
+++ js/app.js
@@ -12,6 +12,8 @@ $(".fancy_title").lettering();
 $(".meat").lettering('lines', 'words');
 $('.alcohols').lettering();
 $('.sebastian').lettering('lines');
+$('.clickMe').lettering();
+// #('.demo').lettering();
 
 
 $(".alcohols *").rotate({bind:{
@@ -36,6 +38,47 @@ $(".sebastian *").rotate({bind:{
   }
 });
 
+//vue js below
+// register the grid component
+Vue.component('demo-grid', {
+  template: '#grid-template',
+  props: {
+    data: Array,
+    columns: Array,
+    filterKey: String
+  },
+  data: function () {
+    var sortOrders = {}
+    this.columns.forEach(function (key) {
+      sortOrders[key] = 1
+    })
+    return {
+      sortKey: '',
+      sortOrders: sortOrders
+    }
+  },
+  methods: {
+    sortBy: function (key) {
+      this.sortKey = key
+      this.sortOrders[key] = this.sortOrders[key] * -1
+    }
+  }
+})
+
+// bootstrap the demo
+var demo = new Vue({
+  el: '#demo',
+  data: {
+    searchQuery: '',
+    gridColumns: ['name', 'power'],
+    gridData: [
+      { name: 'Chuck Norris', power: Infinity },
+      { name: 'Bruce Lee', power: 9000 },
+      { name: 'Jackie Chan', power: 7000 },
+      { name: 'Jet Li', power: 8000 }
+    ]
+  }
+})
 
 
 //document.ready parentheses below
